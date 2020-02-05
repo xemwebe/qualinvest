7556332719f5952039de0ccab97c696697eb3b41
Mark Beinker <mwb@quantlink.de>
2020-02-05 20:33:01 +0100

    First parse transaction test

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
 ///! # qualinvest
 ///! A cloud based tool for quantitative analysis and management of financial asset portfolios
 
-
 pub mod read_pdf;
 use read_pdf::text_from_pdf;
+use read_pdf::read_transaction::parse_transaction;
+#[macro_use] extern crate lazy_static;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 	assert!(args.len() == 2, format!("usage: {} <pdf document>", args[0]) );
     let pdf_file = &args[1];
-    println!("File {} contains:\n{}", pdf_file, text_from_pdf(&pdf_file).unwrap() );
+    let text = text_from_pdf(&pdf_file);
+    match text {
+        Ok(text) => {
+            let result = parse_transaction(&text);
+            match result {
+                Ok((transaction, asset)) =>  {
+                    println!("Could read transaction\n{:?}\non asset\n{:?}", transaction, asset);
+                },
+                Err(err) => {
+                    println!("Reading transaction from parsed pdf failed with error {:?}.", err);
+                },
+            }
+        },
+        Err(err) => {
+            println!("Extracting text from pdf failed with error {}.", err);
+        }
+    }
 }
