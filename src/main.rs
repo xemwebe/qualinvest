6faa371b1fc41e211af9ff0dcee3093c89343be3
Mark Beinker <mwb@quantlink.de>
2020-02-06 16:47:22 +0100

    Config file added

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 ///! # qualinvest
 ///! A cloud based tool for quantitative analysis and management of financial asset portfolios
 
-pub mod read_pdf;
+use serde::Deserialize;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
 use read_pdf::text_from_pdf;
 use read_pdf::read_transactions::parse_transactions;
+use finql::data_handler::DataHandler;
+use finql::postgres_handler::PostgresDB;
+
 #[macro_use] extern crate lazy_static;
 
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-	assert!(args.len() == 2, format!("usage: {} <pdf document>", args[0]) );
-    let pdf_file = &args[1];
-    let text = text_from_pdf(&pdf_file);
+pub mod read_pdf;
+
+
+fn parse_and_store<DB: DataHandler>(pdf_file: &str, db: &mut DB) {
+    let text = text_from_pdf(pdf_file);
     match text {
         Ok(text) => {
             let transactions = parse_transactions(&text);
@@ -36,4 +43,29 @@ fn main() {
             println!("Extracting text from pdf failed with error {}.", err);
         }
     }
+}
+
+/// Configuration parameters
+#[derive(Debug,Deserialize)]
+struct Config {
+    db_host: String,
+    db_user: String,
+    db_password: String,
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+	assert!(args.len() == 2, format!("usage: {} <pdf document>", args[0]) );
+    let pdf_file = &args[1];
+
+    let config_file = File::open("qualinvest.json").unwrap();
+    let config_reader = BufReader::new(config_file);
+    let config: Config = serde_json::from_reader(config_reader).unwrap();
+
+    let connect_str = format!("host={} user={} password={} dbname={} sslmode=disable", config.db_host, config.db_user, config.db_password);
+    let mut db = PostgresDB::connect(connect_str).unwrap();
+    db.clean().unwrap();
+    transaction_tests(&mut db);
+
+    parse_and_store(&pdf_file, &mut db);
 }
