5d37c3301f71b29c5efbffb1d4d9925fbfb18b23
Mark Beinker <mwb@quantlink.de>
2020-02-07 09:22:03 +0100

    config file template added, bugs fixed

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,47 @@
+use finql::postgres_handler::PostgresDB;
+use read_pdf::parse_and_store;
 ///! # qualinvest
 ///! A cloud based tool for quantitative analysis and management of financial asset portfolios
-
 use serde::Deserialize;
-use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
-use std::path::Path;
-use read_pdf::text_from_pdf;
-use read_pdf::read_transactions::parse_transactions;
-use finql::data_handler::DataHandler;
-use finql::postgres_handler::PostgresDB;
 
-#[macro_use] extern crate lazy_static;
+#[macro_use]
+extern crate lazy_static;
 
 pub mod read_pdf;
 
-
-fn parse_and_store<DB: DataHandler>(pdf_file: &str, db: &mut DB) {
-    let text = text_from_pdf(pdf_file);
-    match text {
-        Ok(text) => {
-            let transactions = parse_transactions(&text);
-            match transactions {
-                Ok((transactions, asset)) =>  {
-                    println!("Found underlying\n{:#?}", asset);
-                    if transactions.len() == 0 {
-                        println!("Could not parse any transactions!");
-                    } else {
-                        println!("Found {} transaction{}:", transactions.len(),
-                            if transactions.len()>1 {"s"} else {""});
-                        for trans in transactions {
-                            println!("{:#?}", trans);
-                        }
-                    }
-                },
-                Err(err) => {
-                    println!("Reading transaction from parsed pdf failed with error {:?}.", err);
-                },
-            }
-        },
-        Err(err) => {
-            println!("Extracting text from pdf failed with error {}.", err);
-        }
-    }
-}
-
 /// Configuration parameters
-#[derive(Debug,Deserialize)]
+#[derive(Debug, Deserialize)]
 struct Config {
     db_host: String,
+    db_name: String,
     db_user: String,
     db_password: String,
 }
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-	assert!(args.len() == 2, format!("usage: {} <pdf document>", args[0]) );
+    assert!(
+        args.len() == 2,
+        format!("usage: {} <pdf document>", args[0])
+    );
     let pdf_file = &args[1];
 
     let config_file = File::open("qualinvest.json").unwrap();
     let config_reader = BufReader::new(config_file);
     let config: Config = serde_json::from_reader(config_reader).unwrap();
 
-    let connect_str = format!("host={} user={} password={} dbname={} sslmode=disable", config.db_host, config.db_user, config.db_password);
-    let mut db = PostgresDB::connect(connect_str).unwrap();
-    db.clean().unwrap();
-    transaction_tests(&mut db);
+    let connect_str = format!(
+        "host={} user={} password={} dbname={} sslmode=disable",
+        config.db_host, config.db_user, config.db_password, config.db_name
+    );
+    let mut db = PostgresDB::connect(&connect_str).unwrap();
+    //db.clean().unwrap();
 
-    parse_and_store(&pdf_file, &mut db);
+    let (assets, transactions) = parse_and_store(&pdf_file, &mut db).unwrap();
+    println!(
+        "{} asset(s) and {} transaction(s) stored in database.",
+        assets, transactions
+    );
 }
--- src/read_pdf/mod.rs
+++ src/read_pdf/mod.rs
@@ -1,17 +1,18 @@
-///! # Read pdf files and transform into plain text
-///! This requires the extern tool `pdftotext` 
-///! which is part of [XpdfReader](https://www.xpdfreader.com/pdftotext-man.html).
-
-use std::process::Command;
+use chrono::NaiveDate;
+use finql::currency;
+use finql::data_handler::{DataError, DataHandler};
 use std::error::Error;
 use std::fmt;
-use std::string;
 use std::io;
 use std::num;
-use finql::currency;
-use chrono::NaiveDate;
+///! # Read pdf files and transform into plain text
+///! This requires the extern tool `pdftotext`
+///! which is part of [XpdfReader](https://www.xpdfreader.com/pdftotext-man.html).
+use std::process::Command;
+use std::string;
 
-pub mod read_transactions;
+mod read_transactions;
+use read_transactions::parse_transactions;
 
 #[derive(Debug)]
 pub enum ReadPDFError {
@@ -19,6 +20,7 @@ pub enum ReadPDFError {
     ParseError(string::FromUtf8Error),
     ParseFloat(num::ParseFloatError),
     ParseCurrency(currency::CurrencyError),
+    DBError(DataError),
     ParseDate,
     NotFound,
 }
@@ -49,23 +51,55 @@ impl From<io::Error> for ReadPDFError {
 
 pub fn text_from_pdf(file: &str) -> Result<String, ReadPDFError> {
     let output = Command::new("pdftotext")
-    .arg("-layout")
-    .arg("-q")
-    .arg(&file)
-    .arg("-")
-    .output()?;
+        .arg("-layout")
+        .arg("-q")
+        .arg(&file)
+        .arg("-")
+        .output()?;
     Ok(String::from_utf8(output.stdout)?)
 }
 
-/// Convert a string with German number convention 
+/// Convert a string with German number convention
 /// (e.g. '.' as thousands separator and ',' as decimal separator)
-pub fn german_string_to_float(num_string: &str) -> Result<f64, ReadPDFError>  {
-    num_string.replace(".", "").replace(",",".").parse()
+pub fn german_string_to_float(num_string: &str) -> Result<f64, ReadPDFError> {
+    num_string
+        .replace(".", "")
+        .replace(",", ".")
+        .parse()
         .map_err(|err| ReadPDFError::ParseFloat(err))
 }
 
 /// Converts strings in German data convention to NaiveDate
-pub fn german_string_to_date(date_string: &str) -> Result<NaiveDate, ReadPDFError>  {
-    NaiveDate::parse_from_str(date_string, "%d.%m.%Y")
-        .map_err(|_| ReadPDFError::ParseDate)
+pub fn german_string_to_date(date_string: &str) -> Result<NaiveDate, ReadPDFError> {
+    NaiveDate::parse_from_str(date_string, "%d.%m.%Y").map_err(|_| ReadPDFError::ParseDate)
+}
+
+pub fn parse_and_store<DB: DataHandler>(
+    pdf_file: &str,
+    db: &mut DB,
+) -> Result<(i32, i32), ReadPDFError> {
+    let text = text_from_pdf(pdf_file);
+    match text {
+        Ok(text) => {
+            let transactions = parse_transactions(&text);
+            match transactions {
+                Ok((transactions, asset)) => {
+                    let asset_id = db
+                        .insert_asset_if_new(&asset)
+                        .map_err(|err| ReadPDFError::DBError(err))?;
+                    let mut count = 0;
+                    for trans in transactions {
+                        let mut trans = trans.clone();
+                        trans.set_asset_id(asset_id);
+                        db.insert_transaction(&trans)
+                            .map_err(|err| ReadPDFError::DBError(err))?;
+                        count += 1;
+                    }
+                    Ok((1, count))
+                }
+                Err(err) => Err(err),
+            }
+        }
+        Err(err) => Err(err),
+    }
 }
