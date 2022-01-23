41a172553e61e4d251cff54c585dcf126339a369
Mark Beinker <mwb@quantlink.de>
2022-01-23 22:11:18 +0100

    script for building debian package in docker

---
+++ scripts/make_debian_deployment_package.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+sudo docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/qualinvest -w /usr/src/qualinvest rust cargo build --release
+mkdir -p dist_package
+cd dist_package
+mkdir -p bin
+mkdir -p static
+mkdir -p templates
+mkdir -p config
+cp ../target/release/qualinvest_cli bin
+cp ../target/release/qualinvest_server bin
+cp -R ../qualinvest_server/static/* static
+cp -R ../qualinvest_server/templates/*.tera templates
+cp ../qualinvest_template.toml config
+
+cd ..
+tar czvf qualinvest.tar.gz dist_package/*
+rm -rf dist_package
+
