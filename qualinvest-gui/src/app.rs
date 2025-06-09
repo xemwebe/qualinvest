a786e775bf970a00dd939e606f20930fc6b3b817
Mark Beinker <mwb@quantlink.de>
2025-06-09 20:59:03 +0200

    add custom transaction row renderer to prepare editable table

--- qualinvest-gui/src/app.rs
+++ qualinvest-gui/src/app.rs
@@ -1,10 +1,13 @@
-use crate::transactions;
+use crate::transactions::{TransactionDisplay, TransactionView};
+use leptos::ev::MouseEvent;
 use leptos::prelude::*;
 use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
 use leptos_router::{
     components::{Route, Router, Routes, A},
     StaticSegment,
 };
+use leptos_struct_table::*;
+use std::ops::Range;
 
 pub fn shell(options: LeptosOptions) -> impl IntoView {
     view! {
@@ -45,6 +48,10 @@ pub fn App() -> impl IntoView {
                     <Route path=StaticSegment("") view=HomePage/>
                     <Route path=StaticSegment("transactions") view=|| { view!{ <Transactions/> } }/>
                     <Route path=StaticSegment("position") view=Position/>
+                    <Route path=StaticSegment("assets") view=|| { view!{ <Assets/> } }/>
+                    <Route path=StaticSegment("performance") view=|| { view!{ <Performance/> } }/>
+                    <Route path=StaticSegment("settings") view=|| { view!{ <Settings/> } }/>
+                    <Route path=StaticSegment("accounts") view=|| { view!{ <Accounts/> } }/>
                 </Routes>
             </main>
         </Router>
@@ -103,39 +110,138 @@ fn Transactions() -> impl IntoView {
 }
 
 #[component]
-fn TransactionTable(transactions: Vec<transactions::TransactionView>) -> impl IntoView {
-    let rows = transactions
-        .into_iter()
-        .map(|transaction| {
-            view! {
-            <tr>
-                <td>{transaction.group_id}</td>
-                <td>{transaction.asset_name}</td>
-                <td>{transaction.position}</td>
-                <td>{transaction.trans_type}</td>
-                <td>{transaction.cash_amount}</td>
-                <td>{transaction.cash_currency}</td>
-                <td>{transaction.cash_date}</td>
-                <td>{transaction.doc_path}</td>
-                <td>{transaction.note}</td>
-            </tr> }
-        })
-        .collect_view();
+pub fn TransactionEditButtonRenderer(
+    row: RwSignal<TransactionView>,
+    index: usize,
+) -> impl IntoView {
+    let reload_controller = use_context::<ReloadController>().expect("ReloadController not found");
+    let delete_row_set =
+        use_context::<WriteSignal<Option<i32>>>().expect("delete row signal not found");
+
+    let on_click = move |_| match index {
+        1 => match row.get().state {
+            TransactionDisplay::View => {
+                row.update(|row| row.state = TransactionDisplay::Edit);
+                reload_controller.reload();
+            }
+            TransactionDisplay::Edit => {
+                row.update(|row| row.state = TransactionDisplay::View);
+                reload_controller.reload();
+            }
+        },
+        2 => {
+            delete_row_set.set(Some(row.read().id));
+        }
+        _ => unreachable!(),
+    };
 
     view! {
-        <table>
-            <tr>
-                <th>"Group ID"</th>
-                <th>"Asset Name"</th>
-                <th>"Position"</th>
-                <th>"Type"</th>
-                <th>"Amount"</th>
-                <th>"Currency"</th>
-                <th>"Date"</th>
-                <th>"Documentation"</th>
-                <th>"Note"</th>
+        <td>
+            <img class="icon" width=25 src={move || row.read().state.get_icon(index)} on:click=on_click />
+        </td>
+    }
+}
+
+impl TableDataProvider<TransactionView> for RwSignal<Vec<TransactionView>> {
+    async fn get_rows(
+        &self,
+        _: Range<usize>,
+    ) -> Result<(Vec<TransactionView>, Range<usize>), String> {
+        let transactions = self.read().clone();
+        let len = transactions.len();
+        Ok((transactions, 0..len))
+    }
+
+    async fn row_count(&self) -> Option<usize> {
+        Some(self.get_untracked().len())
+    }
+}
+
+#[allow(non_snake_case)]
+pub fn TransactionRowRenderer(
+    class: Signal<String>,
+    row: RwSignal<TransactionView>,
+    _index: usize,
+    _selected: Signal<bool>,
+    _on_select: EventHandler<web_sys::MouseEvent>,
+) -> impl IntoView {
+    match row.read().state {
+        TransactionDisplay::View => view! {
+            <tr class=class>
+                <td>{move || row.read().group_id}</td>
+                <td>{move || row.read().asset_name.clone()}</td>
+                <td>{move || row.read().position}</td>
+                <td>{move || row.read().trans_type.clone()}</td>
+                <td>{move || row.read().cash_amount}</td>
+                <td>{move || row.read().cash_currency.clone()}</td>
+                <td>{move || row.read().cash_date.clone()}</td>
+                <td>{move || row.read().note.clone()}</td>
+                <TransactionEditButtonRenderer row index=1 />
+                <TransactionEditButtonRenderer row index=2 />
             </tr>
-            {rows}
+        }
+        .into_any(),
+        TransactionDisplay::Edit => view! {
+            <tr class=class>
+                <td>{move || row.read().group_id}</td>
+                <td>{move || row.read().asset_name.clone()}</td>
+                <td>{move || row.read().position}</td>
+                <td>{move || row.read().trans_type.clone()}</td>
+                <td>{move || row.read().cash_amount}</td>
+                <td>{move || row.read().cash_currency.clone()}</td>
+                <td>{move || row.read().cash_date.clone()}</td>
+                <td>{move || row.read().note.clone()}</td>
+                <TransactionEditButtonRenderer row index=1 />
+                <TransactionEditButtonRenderer row index=2 />
+            </tr>
+        }
+        .into_any(),
+    }
+}
+
+#[component]
+fn TransactionTable(transactions: Vec<TransactionView>) -> impl IntoView {
+    let rows = RwSignal::new(transactions);
+
+    let on_change = move |evt: ChangeEvent<TransactionView>| {
+        rows.write()[evt.row_index] = evt.changed_row.get_untracked();
+    };
+
+    let reload_controller = ReloadController::default();
+    let add_row = move |_ev: MouseEvent| {
+        let mut min_id = 0;
+        for row in rows.read().iter() {
+            min_id = min_id.min(row.id);
+        }
+        let new_row = TransactionView::new(min_id - 1);
+        rows.write().push(new_row);
+        reload_controller.reload();
+    };
+    let (delete_row, delete_row_set) = signal::<Option<i32>>(None);
+
+    provide_context(reload_controller);
+    provide_context(delete_row_set);
+
+    Effect::new(move || {
+        if let Some(row_id) = delete_row.get() {
+            let mut index = None;
+            for (i, row) in rows.read().iter().enumerate() {
+                if row.id == row_id {
+                    index = Some(i);
+                    break;
+                }
+            }
+            if let Some(index) = index {
+                rows.write().remove(index);
+                reload_controller.reload();
+            }
+        }
+    });
+
+    view! {
+        <img class="icon" width=25 src="plus.svg" on:click=add_row />
+        <table class="transactions">
+            <TableContent rows row_renderer=TransactionRowRenderer on_change scroll_container="html" />
         </table>
     }
 }
@@ -150,6 +256,71 @@ fn Position() -> impl IntoView {
     }
 }
 
+#[derive(TableRow, Clone)]
+#[table(impl_vec_data_provider)]
+pub struct Person {
+    id: u32,
+    name: String,
+    age: u32,
+}
+
+#[component]
+fn Assets() -> impl IntoView {
+    let rows = vec![
+        Person {
+            id: 1,
+            name: "John".to_string(),
+            age: 32,
+        },
+        Person {
+            id: 2,
+            name: "Jane".to_string(),
+            age: 28,
+        },
+        Person {
+            id: 3,
+            name: "Bob".to_string(),
+            age: 45,
+        },
+    ];
+
+    view! {
+        <div class="center">
+            <h1>Assets</h1>
+            <table>
+                <TableContent rows scroll_container="html" />
+            </table>
+       </div>
+    }
+}
+
+#[component]
+fn Performance() -> impl IntoView {
+    view! {
+        <div class="center">
+            <h1>Performance</h1>
+            <p>Here you can see your performance.</p>
+        </div>
+    }
+}
+#[component]
+fn Settings() -> impl IntoView {
+    view! {
+        <div class="center">
+            <h1>Settings</h1>
+            <p>Here you can see your settings.</p>
+        </div>
+    }
+}
+#[component]
+fn Accounts() -> impl IntoView {
+    view! {
+        <div class="center">
+            <h1>Accounts</h1>
+            <p>Here you can see your accounts.</p>
+        </div>
+    }
+}
 #[component]
 fn Nav() -> impl IntoView {
     let nav_menu = RwSignal::new(false);
@@ -162,6 +333,10 @@ fn Nav() -> impl IntoView {
                     <li class="logo"><A href="/">QuantInvest</A></li>
                     <li class={move || if nav_menu.get() { "show" } else { "" } }><A href="/transactions">Transactions</A></li>
                     <li class={move || if nav_menu.get() { "show" } else { "" } }><A href="/position">Position</A></li>
+                    <li class={move || if nav_menu.get() { "show" } else { "" } }><A href="/assets">Assets</A></li>
+                    <li class={move || if nav_menu.get() { "show" } else { "" } }><A href="/performance">Performance</A></li>
+                    <li class={move || if nav_menu.get() { "show" } else { "" } }><A href="/settings">Settings</A></li>
+                    <li class={move || if nav_menu.get() { "show" } else { "" } }><A href="/accounts">Accounts</A></li>
                 </ul>
                 //<button id="hamburger" on:click=move |_| nav_menu.update(|f| *f=!(*f)) />
             </nav>
